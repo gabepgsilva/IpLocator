f8756d1abcfbf2fd3ed3d4196cc93d83e7cf9fd7
gabepgsilva <gabepgsilva@gmail.com>
2025-06-14 12:34:00 -0700

    Remove invalid functions/api/lookup.js to fix Cloudflare Pages build

--- functions/api/lookup.js
+++
@@ -1,1 +0,0 @@
-Content-Security-Policy = "default-src 'self'; script-src 'self' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://unpkg.com; connect-src 'self' https://ip-api.com;" 
