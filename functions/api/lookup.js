e278fe2fe1cf05a9e5e0644acf3967043ec9e909
gabepgsilva <gabepgsilva@gmail.com>
2025-06-14 09:51:48 -0700

    trigger rebuild

--- functions/api/lookup.js
+++ functions/api/lookup.js
@@ -31,7 +31,6 @@ export async function onRequest(context) {
     headers: apiResponse.headers
   });
 
-  // Add the crucial CORS headers
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
